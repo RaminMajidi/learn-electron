2613e815c150c08a5e5831656b060369cac033cd
RaminMajidi <raminmajididev01@gmail.com>
2024-12-03 13:36:04 +0330

    [change] learn (clipboard and cahenge electron version)

--- src/main.ts
+++ src/main.ts
@@ -1,64 +1,119 @@
-import {
-  app,
-  BrowserWindow,
-  screen,
-  session 
-} from "electron";
-import mainMenu from "./components/menu";
+import { app, BrowserWindow, dialog, globalShortcut, Menu, MenuItem,Tray,powerMonitor,session,desktopCapturer,ipcMain,screen } from "electron";
 import * as path from "path";
-import createAppTray from "./utils/createAppTray";
+
+let appTray,mainWindow:BrowserWindow;
+
+var trayMenu=Menu.buildFromTemplate([
+  {
+    label:'Item 1'
+  },{
+    label:'Item 2'
+  }
+])
+
+var mainMenu = Menu.buildFromTemplate([{
+  label: 'Electron',
+  submenu: [
+    {
+      label: 'Item1',
+      enabled: false
+    },
+    {
+      label: 'Item2',
+      click: () => {
+        console.log(' Item 2 From Electron Is Click');
+      }
+    },
+    {
+      label: 'Item3',
+      accelerator: 'shift + F',
+      click: () => {
+        console.log(' Item 3 From Electron Is Click');
+      }
+    }, {
+      label: 'Item 4',
+      role: 'togglefullscreen'
+    }
+  ]
+},
+{
+  label: 'Edit',
+  submenu: [
+    {
+      label: 'Action 1'
+    }, {
+      label: 'Action 2'
+    }
+  ]
+}
 
 
-
-
-export let mainWindow: BrowserWindow;
-
+]);
 
 function createWindow() {
-
-  // console.log(screen.getAllDisplays());
-  const primaryDisplay = screen.getPrimaryDisplay();
-  
-  session.defaultSession.clearCache().then(()=>{
-    console.log("Cache cleared!");
-  });
-
   // Create the browser window.
+
+  var primaryDisplay=screen.getPrimaryDisplay();
+
   mainWindow = new BrowserWindow({
     height: primaryDisplay.size.height,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
+      nodeIntegration:true
     },
-    width: primaryDisplay.size.width / 2,
-    title: "first page",
-    x: primaryDisplay.bounds.x,
-    y: primaryDisplay.bounds.y,
-    backgroundColor: "#fcba03",
+    width: primaryDisplay.size.width/2,
+    title: 'First Test App',
+    backgroundColor: '#fcba03',
     show: false,
-    alwaysOnTop: true
+    alwaysOnTop: false,
+    x:primaryDisplay.bounds.x,
+    y:primaryDisplay.bounds.y
   });
 
-  createAppTray();
+  CreateAppTray();
+
 
   // and load the index.html of the app.
-  mainWindow.loadFile("../index.html");
+  mainWindow.loadFile(path.join(__dirname, "../index.html"));
+
   mainWindow.setMenu(mainMenu);
 
-  mainWindow.on("ready-to-show", () => {
+  mainWindow.on('ready-to-show', function () {
     mainWindow.show();
   });
 
+  mainWindow.webContents.on('did-finish-load', function () {
+  });
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
+
+}
+
+function CreateAppTray(){
+  let imagePath:string=path.join('assets','app-icon.png');
+  appTray=new Tray(imagePath);
+
+  appTray.setToolTip('My Application');
+
+  appTray.setContextMenu(trayMenu);
+
+  appTray.on('click',function(e){
+
+    if(e.shiftKey){
+      app.quit();
+    }else{
+      mainWindow.isVisible()?mainWindow.hide():mainWindow.show();
+    }
+  });
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-
-app.whenReady().then(() => {
+app.on("ready", () => {
   createWindow();
+
   app.on("activate", function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
@@ -68,14 +123,12 @@ app.whenReady().then(() => {
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
-
 // explicitly with Cmd + Q.
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
     app.quit();
   }
 });
-
 
 // In this file you can include the rest of your app"s specific main process
 // code. You can also put them in separate files and require them here.
--- src/preload.ts
+++ src/preload.ts
@@ -1,6 +1,23 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
+import { ipcRenderer, nativeImage, clipboard } from 'electron';
 
 window.addEventListener("DOMContentLoaded", () => {
-  console.log("Loaded");
+
+  const toUpperBtn = document.getElementById('to-uppercase');
+  const showClipboardTextBtn = document.getElementById('show-clipboard-text');
+  if (toUpperBtn !== null) {
+    toUpperBtn.addEventListener('click', function () {
+      let clipboardText = clipboard.readText();
+      clipboardText = clipboardText.toUpperCase();
+      clipboard.writeText(clipboardText);
+    });
+  }
+
+  if (showClipboardTextBtn !== null) {
+    showClipboardTextBtn.addEventListener('click', function () {
+      console.log(clipboard.readText());
+    });
+  }
+
 });
