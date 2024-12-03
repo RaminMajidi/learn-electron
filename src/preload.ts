e3f1529bb31e55fd7c8739b20ccae5969a83e68a
RaminMajidi <raminmajididev01@gmail.com>
2024-12-03 21:07:23 +0330

    [delete] all file

--- src/preload.ts
+++
@@ -1,7 +0,0 @@
-// All of the Node.js APIs are available in the preload process.
-// It has the same sandbox as a Chrome extension.
-import { ipcRenderer, nativeImage, clipboard, shell } from 'electron';
-
-window.addEventListener("DOMContentLoaded", () => {
-
-});
