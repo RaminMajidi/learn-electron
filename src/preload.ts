c8a01dc5b369921cefdcfcf3b44d979a2e789289
RaminMajidi <raminmajididev01@gmail.com>
2024-12-03 14:00:11 +0330

    [change] learn (shell)

--- src/preload.ts
+++ src/preload.ts
@@ -1,23 +1,21 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
-import { ipcRenderer, nativeImage, clipboard } from 'electron';
+import { ipcRenderer, nativeImage, clipboard, shell } from 'electron';
 
 window.addEventListener("DOMContentLoaded", () => {
 
-  const toUpperBtn = document.getElementById('to-uppercase');
-  const showClipboardTextBtn = document.getElementById('show-clipboard-text');
-  if (toUpperBtn !== null) {
-    toUpperBtn.addEventListener('click', function () {
-      let clipboardText = clipboard.readText();
-      clipboardText = clipboardText.toUpperCase();
-      clipboard.writeText(clipboardText);
-    });
-  }
+ const btnOpenElectron = <HTMLButtonElement>document.getElementById("btnOpenElectron");
+ const btnOpenImage = <HTMLButtonElement>document.getElementById("btnOpenImage");
+ const btnOpenFolder = <HTMLButtonElement>document.getElementById("btnOpenFolder");
 
-  if (showClipboardTextBtn !== null) {
-    showClipboardTextBtn.addEventListener('click', function () {
-      console.log(clipboard.readText());
-    });
-  }
-
+ btnOpenElectron.addEventListener('click',function(){
+  shell.openExternal('https://www.electronjs.org');
+ });
+ btnOpenImage.addEventListener('click',function(){
+  shell.openPath('my-picture.jpg');
+ });
+ btnOpenFolder.addEventListener('click',function(){
+  shell.showItemInFolder('E:\\exampel\\electron\\learn-electron\\my-picture.jpg');
+ });
+ 
 });
